34eb162d699816d681cca2525da8058d1465b13a
mahaalghadban64 <mahaalghadban64@gmail.com>
2025-10-17 13:59:13 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,4 @@
-// sidebar functions
+  // sidebar functions
 function showsidebar() {
   const sidebar = document.querySelector('.sidebar');
   sidebar.style.display = 'flex';
@@ -22,13 +22,38 @@ document.querySelector('.sidebar .close').addEventListener('click', hidesidebar)
       const sidebar = document.querySelector('.sidebar');
       sidebar.style.display = 'none';
     }
+    // ------------------------------------------------------------------
+      // sidebar functions
+function showsidebarar() {
+  const sidebarar = document.querySelector('.arabic .sidebar');
+  sidebarar.style.display = 'flex';
+}
 
-    // Add event listeners
-    document.querySelector('.menu-toggle').addEventListener('click', showsidebar);
-    document.querySelector('.sidebar .close').addEventListener('click', hidesidebar);
+function hidesidebarar() {
+  const sidebarar = document.querySelector('.arabic .sidebar');
+  sidebarar.style.display = 'none';
+}
 
- 
+// Add event listeners
+document.querySelector('.arabic .menu-toggle').addEventListener('click', showsidebarar);
+document.querySelector('.arabic .sidebar .close').addEventListener('click', hidesidebarar);
 
+    function showsidebarar() {
+      const sidebarar = document.querySelector('.arabic .sidebar');
+      sidebarar.style.display = 'flex';
+    }
+
+    function hidesidebarar() {
+      const sidebarar = document.querySelector('.arabic .sidebar');
+      sidebarar.style.display = 'none';
+    }
+
+
+
+  
+
+     // ------------------------------------------------------------------
+// slider section 
 var indexValue =1;
 showImg(indexValue);     
 
@@ -71,6 +96,74 @@ sliders[i].style.background="white";
 img[indexValue -1].style.display ="block";
 sliders[indexValue -1].style.background="#500d74";
 }
+// --------------------------------------------------------------
+  document.querySelector('.menu-toggle').addEventListener('click', showsidebar);
+    document.querySelector('.sidebar .close').addEventListener('click', hidesidebar);
+
+ 
+
+var indexValue =1;
+showImgar(indexValue);     
+
+function btm_slidear(o){
+showImgar(indexValue =o);}
+
+function side_slidear(o){
+showImgar(indexValue+=o);}
+
+
+function showImgar(o){
+var i;
+const imgar =document.querySelectorAll('.arabic .trading-event .slides img');
+const slidersar=document.querySelectorAll('.arabic .trading-event .btm-sliders span');
+
+
+
+
+
+
+
+if(o > imgar.length){indexValue=1}
+if(o < 1){indexValue=img.length}
+
+
+for(i = 0; i< imgar.length; i++)
+{
+imgar[i].style.display="none";
+
+}
+
+
+for(i=0;i<slidersar.length;i++)
+{
+slidersar[i].style.background="white";
+}
+
+
+imgar[indexValue -1].style.display ="block";
+slidersar[indexValue -1].style.background="#500d74";
+}
+
+
+
+
+// -------------------------------------------------------------------
+function setLanguage(lang){
+  if(lang=='ar'){
+    document.body.setAttribute('lang','ar');
+  document.body.setAttribute('dir', 'rtl');
+
+  }
+  
+  else{
+     document.body.setAttribute('lang','en');
+  document.body.setAttribute('dir', 'ltr');
+  }
+}
+
+
+
+ 
 
 
 
