4897dc0b1c1b62b933601bd9f4caf9f2a7263eb0
mahaalghadban64 <mahaalghadban64@gmail.com>
2025-10-23 00:50:21 +0300

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,56 @@
+  // sidebar functions
+function showsidebar() {
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'flex';
+}
+
+function hidesidebar() {
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'none';
+}
+
+// Add event listeners
+document.querySelector('.menu-toggle').addEventListener('click', showsidebar);
+document.querySelector('.sidebar .close').addEventListener('click', hidesidebar);
+
+    function showsidebar() {
+      const sidebar = document.querySelector('.sidebar');
+      sidebar.style.display = 'flex';
+    }
+
+    function hidesidebar() {
+      const sidebar = document.querySelector('.sidebar');
+      sidebar.style.display = 'none';
+    }
+    // ------------------------------------------------------------------
+      // sidebar functions
+function showsidebarar() {
+  const sidebarar = document.querySelector('.arabic .sidebar');
+  sidebarar.style.display = 'flex';
+}
+
+function hidesidebarar() {
+  const sidebarar = document.querySelector('.arabic .sidebar');
+  sidebarar.style.display = 'none';
+}
+
+// Add event listeners
+document.querySelector('.arabic .menu-toggle').addEventListener('click', showsidebarar);
+document.querySelector('.arabic .sidebar .close').addEventListener('click', hidesidebarar);
+
+    function showsidebarar() {
+      const sidebarar = document.querySelector('.arabic .sidebar');
+      sidebarar.style.display = 'flex';
+    }
+
+    function hidesidebarar() {
+      const sidebarar = document.querySelector('.arabic .sidebar');
+      sidebarar.style.display = 'none';
+    }
+
+
+
+
 document.querySelectorAll(".massegeForm").forEach(form => {
   form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -23,4 +76,37 @@ document.querySelectorAll(".massegeForm").forEach(form => {
     alert("Your message has been sent successfully! you wil receive a reply soon");
     form.reset();
   });
+});
+// =======================================================
+
+function setLanguage(lang){
+  if(lang=='ar'){
+    document.body.setAttribute('lang','ar');
+  document.body.setAttribute('dir', 'rtl');
+
+  }
+  
+  else{
+     document.body.setAttribute('lang','en');
+  document.body.setAttribute('dir', 'ltr');
+  }
+}
+// dark mode
+
+  const toggles = document.querySelectorAll('.theme-toggle');
+
+  toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+      document.body.classList.toggle('dark-mode');
+
+    });
+  });
+
+toggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  if (document.body.classList.contains('dark-mode')) {
+    localStorage.theme = 'dark';
+  } else {
+    localStorage.theme = 'light';
+  }
 });
