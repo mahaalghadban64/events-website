b2cd6333584c07fff7ada95e88604b46abc1f5c4
mahaalghadban64 <mahaalghadban64@gmail.com>
2025-10-23 00:49:50 +0300

    Update about.js

--- js/about.js
+++ js/about.js
@@ -1,3 +1,54 @@
+  // sidebar functions
+function showsidebar() {
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'flex';
+}
+
+function hidesidebar() {
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'none';
+}
+
+// Add event listeners
+document.querySelector('.menu-toggle').addEventListener('click', showsidebar);
+document.querySelector('.sidebar .close').addEventListener('click', hidesidebar);
+
+    function showsidebar() {
+      const sidebar = document.querySelector('.sidebar');
+      sidebar.style.display = 'flex';
+    }
+
+    function hidesidebar() {
+      const sidebar = document.querySelector('.sidebar');
+      sidebar.style.display = 'none';
+    }
+    // ------------------------------------------------------------------
+      // sidebar functions
+function showsidebarar() {
+  const sidebarar = document.querySelector('.arabic .sidebar');
+  sidebarar.style.display = 'flex';
+}
+
+function hidesidebarar() {
+  const sidebarar = document.querySelector('.arabic .sidebar');
+  sidebarar.style.display = 'none';
+}
+
+// Add event listeners
+document.querySelector('.arabic .menu-toggle').addEventListener('click', showsidebarar);
+document.querySelector('.arabic .sidebar .close').addEventListener('click', hidesidebarar);
+
+    function showsidebarar() {
+      const sidebarar = document.querySelector('.arabic .sidebar');
+      sidebarar.style.display = 'flex';
+    }
+
+    function hidesidebarar() {
+      const sidebarar = document.querySelector('.arabic .sidebar');
+      sidebarar.style.display = 'none';
+    }
+// =======================================================
+
 function setLanguage(lang){
   if(lang=='ar'){
     document.body.setAttribute('lang','ar');
@@ -10,3 +61,22 @@ function setLanguage(lang){
   document.body.setAttribute('dir', 'ltr');
   }
 }
+// dark mode
+
+  const toggles = document.querySelectorAll('.theme-toggle');
+
+  toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+      document.body.classList.toggle('dark-mode');
+
+    });
+  });
+
+toggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  if (document.body.classList.contains('dark-mode')) {
+    localStorage.theme = 'dark';
+  } else {
+    localStorage.theme = 'light';
+  }
+});
